9773d3cb9f1cbe04b48d780c79e7726660123cfa
lucasgabrieldesouzacabral <lgsc5@discente.ifpe.edu.br>
2025-12-08 15:47:26 -0300

    .

--- models/vacina.model.js
+++ models/vacina.model.js
@@ -2,21 +2,9 @@ const {DataTypes} = require('sequelize');
 const sequelize = require('../config/database');
 
 const Vacina = sequelize.define('Vacina', {
-    nome: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    tipo: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
     descricao: {
         type: DataTypes.STRING,
         allowNull: true,
-    },
-    dataAplicacao: {
-        type: DataTypes.STRING,
-        allowNull: false,
     },
     animalId: {
         type: DataTypes.INTEGER,
